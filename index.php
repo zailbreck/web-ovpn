3fee37173412e2dc315857f74077c32bf9f5019d
zailbreck <no081574327482@gmail.com>
2021-11-30 15:21:41 +0700

    Update Animation

--- index.php
+++ index.php
@@ -78,9 +78,11 @@
                 data: { nim : nim_mhs },
                 beforeSend: function() { 
                   $('#container').html("<img src='https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1' />");
+                  $('#modal').openModal();
                 },
                 success: function(response){
                     // alert(response)
+                    $('#modal').closeModal();
                     $('#urlDownload').html(response);
                     $('#modal').openModal();
                 }
